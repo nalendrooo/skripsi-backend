1cadcbe8783c873f0c4449e9e3c2338ac0135bc4
Nalendrooo <nalendroadil274@gmail.com>
2025-06-26 23:49:05 +0700

    a

--- src/setup/web.ts
+++ src/setup/web.ts
@@ -4,7 +4,7 @@ import { errorHandler } from "../middleware/error-handler";
 import cors from 'cors'
 export const web = express()
 
-web.use(cors({ origin: ['http://localhost:5001', 'http:///34.126.178.89:5001', 'http://34.126.178.89:5001/'], credentials: true }))
+web.use(cors({ origin: ['http://localhost:5001', 'http://34.126.178.89:5001', 'http://34.126.178.89:5001/'], credentials: true }))
 web.use(express.json())
 web.use('/api', publicRouter)
 web.use(errorHandler)
